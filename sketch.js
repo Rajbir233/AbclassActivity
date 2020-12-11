b57ce8ec35b6eafd7ffcf0ad24ed428163c7bdad
Rajbir Kaur <rajbir.kaur@blueyonder.com>
2020-12-11 20:37:48 +0530

    Rajbir- added comment

--- sketch.js
+++ sketch.js
@@ -20,6 +20,8 @@ function setup(){
     ground = new Ground(600,height,1200,20);
     platform = new Ground(200,350,400,200);
 
+    //writin a comment
+
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
